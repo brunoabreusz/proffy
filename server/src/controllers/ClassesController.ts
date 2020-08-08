6684c5a93daa94394fc30043e9f745a9fa030280
Bruno <bruno.souza2001@gmail.com>
2020-08-08 11:39:50 -0300

    feat: edit on the backend / frontend

--- server/src/controllers/ClassesController.ts
+++ server/src/controllers/ClassesController.ts
@@ -28,17 +28,16 @@ export default class ClassController {
 
         const classes = await db('classes')
         .whereExists(function(){
-            this.select('class_schedule.*')
-                .from('class_schedule')
-                .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
-                .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
-                .whereRaw('`class_schedule`.`from` <= ?', [timeInMinutes])
-                .whereRaw('`class_schedule`.`to` > ?', [timeInMinutes])
-        })
-        .where('classes.subject', '=', subject)
-        .join('users', 'classes.user_id', '=', 'user.id')
-        .select(['classes.*', 'user.*']);
-
+          this.select('class_schedule.*')
+            .from('class_schedule')
+            .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+            .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+            .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+            .whereRaw('`class_schedule`.`to` > ??', [timeInMinutes])
+          })
+          .where('classes.subject', '=', subject)
+          .join('users', 'classes.user_id', '=', 'users.id')
+          .select(['classes.*', 'users.*']);
         return response.json(classes);
     }
 
